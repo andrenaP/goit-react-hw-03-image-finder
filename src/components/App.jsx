e63d5fca5e81fa6fc49cac56abb8a4b649b8eae6
andrenaP <a0993100106@gmail.com>
2023-07-31 12:47:39 +0300

    Add folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem';
+import Loader from './Loader';
+import Button from './Button';
+import Modal from './Modal';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return <div>{'hi'}</div>;
+  }
+}
+
+export default App;
