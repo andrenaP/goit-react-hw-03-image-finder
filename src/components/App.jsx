de69b4977f9ab3a81eb37d1f0bae28b018154d98
andrenaP <a0993100106@gmail.com>
2023-08-05 20:35:26 +0300

    finall

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import Notiflix from 'notiflix';
+import axios from 'axios';
 
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
+import Loader from './Loader';
+import Button from './Button';
+
+const perPage = 12;
+const API_KEY = '37324716-561554bcb566b8f1f5945e5c9';
+const BASE_URL = 'https://pixabay.com/api/';
 
 class App extends Component {
   state = {
     searchQuery: '',
+    galleryItems: [],
     page: 1,
+    loadMore: false,
+    status: null,
   };
+
+  fetchImages = async () => {
+    const { page, searchQuery } = this.state;
+
+    const result = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+
+    const parseResponse = await result;
+    return parseResponse;
+  };
+  async componentDidUpdate(prevProps, prevState) {
+    const { page, searchQuery } = this.state;
+    const differentSearchQuery = prevState.searchQuery !== searchQuery;
+    const differentPage = prevState.page !== page;
+    if (differentSearchQuery || differentPage) {
+      this.setState({ status: 'pending' });
+      try {
+        const parseResponse = await this.fetchImages();
+
+        this.setState(prevState => ({
+          galleryItems: [...prevState.galleryItems, ...parseResponse.data.hits],
+          status: 'resolved',
+          loadMore: page < Math.ceil(parseResponse.data.totalHits / 12),
+        }));
+        if (parseResponse.data.hits.length === 0) {
+          Notiflix.Notify.failure(
+            `Sorry, there are no images matching search query ${this.props.searchQuery}`
+          );
+        }
+      } catch (error) {
+        Notiflix.Notify.failure(error);
+        this.setState({ status: 'rejected' });
+      }
+    }
+  }
 
   incrementPage = () => {
     this.setState(prevState => ({
@@ -16,18 +63,19 @@ class App extends Component {
   };
 
   handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery, page: 1 });
+    this.setState({ searchQuery, page: 1, galleryItems: [] });
   };
 
   render() {
+    const { loadMore, status, galleryItems } = this.state;
     return (
       <div>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          searchQuery={this.state.searchQuery}
-          page={this.state.page}
-          incrementPage={this.incrementPage}
-        />
+        {status === 'pending' && <Loader />}
+        {galleryItems.length > 0 && (
+          <ImageGallery galleryItems={galleryItems} />
+        )}
+        {loadMore && <Button onClick={this.incrementPage}></Button>}
       </div>
     );
   }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,24 +1,13 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import './ImageGallery.css';
-import axios from 'axios';
-import Notiflix from 'notiflix';
-import Button from '../Button';
-import ImageGalleryItem from '../ImageGalleryItem';
-import Modal from '../Modal';
-import Loader from '../Loader';
-
-const perPage = 12;
-const API_KEY = '37324716-561554bcb566b8f1f5945e5c9';
-const BASE_URL = 'https://pixabay.com/api/';
+import ImageGalleryItem from 'components/ImageGalleryItem';
+import Modal from 'components/Modal';
 
 class ImageGallery extends Component {
   state = {
-    galleryItems: [],
-    status: null,
-    page: 1,
-    loadMore: false,
     showModal: false,
+    largeImageURL: null,
   };
   handleImageClick = largeImageURL => {
     this.setState({
@@ -32,79 +21,33 @@ class ImageGallery extends Component {
     });
   };
 
-  handleIncrementPage = () => {
-    const { incrementPage } = this.props;
-    incrementPage();
-  };
-  async componentDidUpdate(prevProps, prevState) {
-    const { page, searchQuery } = this.props;
-    if (prevProps.searchQuery !== searchQuery) {
-      this.resetPage();
-    }
-
-    if (prevProps.searchQuery !== searchQuery || prevProps.page !== page) {
-      this.setState({ status: 'pending' });
-
-      try {
-        const result = await axios.get(
-          `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-        );
-
-        const parseResponse = await result;
-        this.setState(prevState => ({
-          galleryItems: [...prevState.galleryItems, ...parseResponse.data.hits],
-          status: 'resolved',
-          loadMore: page < Math.ceil(parseResponse.data.totalHits / 12),
-        }));
-      } catch (error) {
-        Notiflix.Notify.failure(error);
-        this.setState({ status: 'rejected' });
-      }
-    }
-  }
-  resetPage() {
-    this.setState({ galleryItems: [], page: 1 });
-  }
   render() {
-    const { galleryItems, status, loadMore, showModal } = this.state;
-    if (status === 'pending') return <Loader />;
-    if (status === 'rejected') {
-      Notiflix.Notify.failure('Oops, something went wrong! Please, try again!');
-      return;
-    }
-    if (status === 'resolved')
-      if (galleryItems.length === 0) {
-        Notiflix.Notify.failure(
-          `Sorry, there are no images matching search query ${this.props.searchQuery}`
-        );
-        return;
-      }
-    console.log(loadMore);
+    const { galleryItems } = this.props;
+    const { showModal } = this.state;
+
     return (
-      <div>
-        {status === 'resolved' && (
-          <ul className="galleryItems">
-            {galleryItems.map(({ id, webformatURL, tags, largeImageURL }) => {
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  smallPicture={webformatURL}
-                  alt={tags}
-                  onClick={this.handleImageClick}
-                  largeImage={largeImageURL}
-                />
-              );
-            })}
-          </ul>
-        )}
+      <>
+        <ul className="galleryItems">
+          {galleryItems.map(({ id, webformatURL, tags, largeImageURL }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                smallPicture={webformatURL}
+                alt={tags}
+                onClick={this.handleImageClick}
+                largeImage={largeImageURL}
+              />
+            );
+          })}
+        </ul>
+
         {showModal && (
           <Modal
             largeImage={this.state.largeImageURL}
             onModalClose={this.onModalClose}
           />
         )}
-        {loadMore && <Button onClick={this.handleIncrementPage}></Button>}
-      </div>
+      </>
     );
   }
 }
@@ -112,7 +55,5 @@ class ImageGallery extends Component {
 export default ImageGallery;
 
 ImageGallery.propTypes = {
-  searchQuery: PropTypes.string,
-  page: PropTypes.number.isRequired,
-  incrementPage: PropTypes.func.isRequired,
+  galleryItems: PropTypes.arrayOf(PropTypes.object.isRequired),
 };
