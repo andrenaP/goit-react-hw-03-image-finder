cf3108551316f41451be7b0146f2f50d9dd7457f
andrenaP <a0993100106@gmail.com>
2023-08-07 13:07:03 +0300

    final fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import Notiflix from 'notiflix';
-import axios from 'axios';
 
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Loader from './Loader';
 import Button from './Button';
-
-const perPage = 12;
-const API_KEY = '37324716-561554bcb566b8f1f5945e5c9';
-const BASE_URL = 'https://pixabay.com/api/';
+import { fetchImages } from './Service';
 
 class App extends Component {
   state = {
@@ -20,16 +16,6 @@ class App extends Component {
     status: null,
   };
 
-  fetchImages = async () => {
-    const { page, searchQuery } = this.state;
-
-    const result = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
-
-    const parseResponse = await result;
-    return parseResponse;
-  };
   async componentDidUpdate(prevProps, prevState) {
     const { page, searchQuery } = this.state;
     const differentSearchQuery = prevState.searchQuery !== searchQuery;
@@ -37,18 +23,19 @@ class App extends Component {
     if (differentSearchQuery || differentPage) {
       this.setState({ status: 'pending' });
       try {
-        const parseResponse = await this.fetchImages();
+        const parseResponse = await fetchImages(page, searchQuery);
+        if (parseResponse.data.hits.length === 0) {
+          Notiflix.Notify.failure(
+            `Sorry, there are no images matching search query ${this.props.searchQuery}`
+          );
+          return;
+        }
 
         this.setState(prevState => ({
           galleryItems: [...prevState.galleryItems, ...parseResponse.data.hits],
           status: 'resolved',
           loadMore: page < Math.ceil(parseResponse.data.totalHits / 12),
         }));
-        if (parseResponse.data.hits.length === 0) {
-          Notiflix.Notify.failure(
-            `Sorry, there are no images matching search query ${this.props.searchQuery}`
-          );
-        }
       } catch (error) {
         Notiflix.Notify.failure(error);
         this.setState({ status: 'rejected' });
